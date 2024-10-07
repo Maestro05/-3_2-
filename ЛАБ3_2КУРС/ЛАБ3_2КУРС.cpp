26845f2fadfdf51f438bf7de93c61316ea8d8bee
Nguyen Tuan Ann <nguyen.05@inbox.ru>
2024-10-07 22:10:45 +0700

    Добавил частично 2 класс

--- ЛАБ3_2КУРС/ЛАБ3_2КУРС.cpp
+++ ЛАБ3_2КУРС/ЛАБ3_2КУРС.cpp
@@ -3,6 +3,18 @@
 #include <iostream>
 using namespace std;
 
+int Select(int a, int b, char text[])
+{
+    int selection;
+    do
+    {
+        printf("\n%s: ", text);
+        scanf("%d", &selection);
+    } while (selection < a || selection > b);
+
+    return selection;
+}
+
 class Food
 {
 private:
@@ -12,12 +24,38 @@ private:
         calories,
         proteins,
         carbohydrates,
-        fats;
+        fats,
+        price;
+public:
+    friend int Select(int a, int b, char text[]);
+
+    void Infromation()
+    {
+        cout << "Информация о еде " << this->name << endl
+            << "Белки: " << this->proteins << endl
+            << "Углеводы: " << this->carbohydrates << endl
+            << "Жиры: " << this->fats << endl
+            << "Вес: " << this->weight << endl
+            << "Каллорий: " << this->calories << endl
+            << "\n----------------------\n1.Заказать\n\n2.Отмена\n";
+        int choice = Select(1, 2, "Выбор действия");
+    }
+
+};
+class Order
+{
+private:
     int
         count,
-        number;
+        number,
+        sum;
 public:
-    void Take()
+    Order()
+    {
+        sum = count = 0;
+        number = 1;
+    }
+    void Korzinka(Food &order)
     {
 
     }
